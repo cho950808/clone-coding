f521b84c714f6f204aefdbac5414a6546834f98b
cho950808 <cho950808@naver.com>
2022-05-09 15:02:37 +0900

    tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: "#e7473c",
+      secondary: "",
+      accent: "",
+      info: "",
+      warning: "",
+      danger: "",
+      light: "",
+      dark: "",
+    },
     extend: {},
   },
   plugins: [],
