717b79cb4a49aa332a64f047372f53a7eccc554f
cho950808 <cho950808@naver.com>
2022-05-10 20:09:03 +0900

    tailwind CSS 8번 #2

--- pages/8/index.js
+++ pages/8/index.js
@@ -1,13 +1,13 @@
 export default function Donicalda() {
   return (
-    <div className="flex flex-col items-start px-[40px] h-screen w-full text-[#444444] m-auto">
+    <div className="flex flex-col items-start px-[40px] h-screen w-full text-[#444444]">
       <section className="w-full mt-[25px]">
         <h1 className="text-[36px]">
           Donica Ida is an art director and designer working in editorial,
           identity, and digital design.
         </h1>
       </section>
-      <section className="mt-[50px] w-full m-auto">
+      <section className="mt-[150px] w-full">
         <div className="flex flex-rows justify-between">
           <div className="text-[12px] basis-1/5 px-2 px-2">
             <label className="">Year</label>
@@ -32,7 +32,12 @@ export default function Donicalda() {
           <div className="text-[20px] basis-1/2 px-2 px-2">
             <label>Identity, website</label>
           </div>
-          <div className="text-[20px] basis-1/2 px-2"></div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
         </div>
         <hr className="border-1.5 border-black" />
 
@@ -46,7 +51,183 @@ export default function Donicalda() {
           <div className="text-[20px] basis-1/2 px-2">
             <label>Identity, editorial</label>
           </div>
-          <div className="text-[20px] basis-1/2 px-2"></div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2018</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">Airbnb</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Vision work</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show_locked.svg"
+              className="h-2.5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2018</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">Condé Nast Traveler</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Vision work</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2018</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">BMW</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Website</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2017</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">Mary Review</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Identity, magazine</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2017</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">Clara Lionel Foundation</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Identity, website</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2017</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">Marriott Magazine</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Website</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2016</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">McKinsey Digital</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Website</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show_locked.svg"
+              className="h-2.5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2015</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">One Hundred Agency</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Website</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/show.svg"
+              className="h-5"
+            />
+          </div>
+        </div>
+        <hr className="border-1.5 border-black" />
+
+        <div className="flex flex-rows justify-between py-4">
+          <div className="text-[20px] basis-1/5 px-2">
+            <label className="">2014</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label className="">North</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2">
+            <label>Identity, app</label>
+          </div>
+          <div className="text-[20px] basis-1/2 px-2 flex items-center justify-end">
+            <img
+              src="http://donicaida.com/style/img/hide.svg"
+              className="h-5"
+            />
+          </div>
         </div>
         <hr className="border-1.5 border-black" />
       </section>
@@ -74,13 +255,28 @@ export default function Donicalda() {
           while looking for new opportunities*.
           <label className="text-[12px]">Connect</label>
           <br />
-          <a className="underline underline-offset-4">Email</a>,&nbsp;
-          <a className="underline underline-offset-4">Instagram</a>,&nbsp;
-          <a className="underline underline-offset-4">LinkedIn</a>,&nbsp;
-          <a className="underline underline-offset-4">Workin</a>g
-          <a className="underline underline-offset-4">NotWorkin</a>g.
+          <a className="underline underline-offset-4 hover:italic" href="#">
+            Email
+          </a>
+          ,&nbsp;
+          <a className="underline underline-offset-4 hover:italic" href="#">
+            Instagram
+          </a>
+          ,&nbsp;
+          <a className="underline underline-offset-4 hover:italic" href="#">
+            LinkedIn
+          </a>
+          ,&nbsp;
+          <a className="underline underline-offset-4 hover:italic" href="#">
+            Workin
+          </a>
+          g
+          <a className="underline underline-offset-4 hover:italic" href="#">
+            NotWorkin
+          </a>
+          g.
         </h2>
-        <label className="text-[12px] my-[20px]">
+        <label className="text-[12px] mt-[60px]">
           *2021 portfolio update coming shortly. Email for latest work. Mahalo.
         </label>
       </section>
